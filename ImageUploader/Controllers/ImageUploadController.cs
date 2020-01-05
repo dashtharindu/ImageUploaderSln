afb85fac1acfa6d4658412bad6bc9ce94ba433cb
dashtharindu <dashtharindu@gmail.com>
2020-01-05 19:02:28 +0530

    separate modules,much cleaner controller, and make async

--- ImageUploader/Controllers/ImageUploadController.cs
+++ ImageUploader/Controllers/ImageUploadController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ImageUploader.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,45 +13,18 @@ namespace ImageUploader.Controllers
     [ApiController]
     public class ImageUploadController : ControllerBase
     {
-        //todo-create controller and put this 1
+        private readonly UploadRepo UploadRepo;
 
-        public static IWebHostEnvironment _environment;
-
-        //constructor
         public ImageUploadController(IWebHostEnvironment environment)
         {
-            _environment = environment;
+            UploadRepo = new UploadRepo(environment);
         }
 
 
         [HttpPost]
-        public async Task<IActionResult> Post(IFormFile file)
+        public async Task<IActionResult> Post(IFormFile img)
         {
-            try
-            {
-                if (file.Length > 0)
-                {
-                    if (!Directory.Exists(_environment.WebRootPath + "\\uploads\\"))
-                    {
-                        Directory.CreateDirectory(_environment.WebRootPath + "\\uploads\\");
-                    }
-                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\uploads\\" + file.FileName))
-                    {
-                        file.CopyTo(fileStream);
-                        fileStream.Flush();
-                        return Ok("\\uploads\\" + file.FileName.ToString());
-                    }
-                }
-                else
-                {
-                    return Ok("failed");
-                }
-            }
-            catch (Exception ex)
-            {
-
-                return Ok(ex.Message.ToString());
-            }
+            return Ok(await UploadRepo.ImageUploader(img));
         }
     }
 }
---
+++ ImageUploader/Data/UploadRepo.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageUploader.Data
+{
+    public class UploadRepo
+    {
+        public static IWebHostEnvironment _environment;//needs using Microsoft.AspNetCore.Hosting;
+
+        public UploadRepo(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public async Task<string> ImageUploader(IFormFile Imagefile)
+        {
+            try
+            {
+                if (Imagefile.Length > 0)
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "\\uploads\\"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "\\uploads\\");
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\uploads\\" + Imagefile.FileName))
+                    {
+                        await Imagefile.CopyToAsync(fileStream);
+                        await fileStream.FlushAsync();
+                        return "/uploads/" + Imagefile.FileName.ToString();
+                    }
+                }
+                else
+                {
+                    return "failed";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
+
+        }
+       
+    }
+}
